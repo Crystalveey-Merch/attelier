7bba38b8b56636154de16605a458ef4a3125cbc5
Udofa Daniel <danieludofa452@gmail.com>
2023-09-19 08:21:51 +0100

    image test

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,35 +3,35 @@ export const datas = {
   newItems: [
     {
       id: 1,
-      image: "/Images/newArrivals/image9.jpeg",
+      image: "/src/Images/newArrivals/image9.jpeg",
       title: "Purple Dress",
       category: "Women Dresses",
-      price: "$ 120.00",
+      price: "N 120.00",
     },
     {
       id: 2,
-      image: "/Images/newArrivals/image14.jpeg",
+      image: "/src/Images/newArrivals/image14.jpeg",
       title: "Brown Trouser",
       category: "Trouser",
       price: "$ 110.00",
     },
     {
       id: 3,
-      image: "/Images/newArrivals/image13.jpeg",
+      image: "/src/Images/newArrivals/image13.jpeg",
       title: "Pink Jacket",
       category: "Coat & Jacket",
       price: "$ 130.00",
     },
     {
       id: 4,
-      image: "/Images/newArrivals/image11.jpeg",
+      image: "/src/Images/newArrivals/image11.jpeg",
       title: "Black Turtle Neck and Jacket",
       category: "Coat & Jacket",
       price: "$ 140.00",
     },
     {
       id: 5,
-      image: "/Images/newArrivals/image12.jpeg",
+      image: "/src/Images/newArrivals/image12.jpeg",
       title: "Black Turtle Neck and Jacket",
       category: "Coat & Jacket",
       price: "$ 140.00",
@@ -99,26 +99,26 @@ export const datas = {
     {
       id: 1,
       name: "Children",         
-      image: "/Images/sec1/image4.png",
+      image: "/src/Images/sec1/image4.png",
       
         
     },
     {
       id: 2,
       name: "Women",         
-      image: "/Images/sec1/image3.png",
+      image: "/src/Images/sec1/image3.png",
         
     },
     {
       id: 3,
       name: "Men",         
-      image: "/Images/sec1/image1.jpeg",
+      image: "/src/Images/sec1/image1.jpeg",
         
     },
     {
       id: 4,
       name: "Accessories",         
-      image: "/Images/sec1/image4.png",
+      image: "/src/Images/sec1/image4.png",
         
     },
   ],
